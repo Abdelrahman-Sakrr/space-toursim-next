fb941533b181713e6f9f485dd2cf45a3b9bc45b9
Abdelrahman-Sakrr <jamessakrrr@gmail.com>
2025-07-27 13:19:42 +0300

    create technology page

---
+++ src/app/technology/CustomTechnologySwipper.tsx
@@ -0,0 +1,105 @@
+"use client";
+import React, { useState, useRef } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import vehicle from "@/../public/technology/image-launch-vehicle-landscape.jpg";
+import capsule from "@/../public/technology/image-space-capsule-landscape.jpg";
+import Image from "next/image";
+import { Swiper as SwiperType } from "swiper";
+
+const crew = [
+  {
+    name: "Space capsule",
+    description:
+      "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
+    image: capsule,
+  },
+  {
+    name: "Launch Vehicle",
+    description:
+      "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
+    image: vehicle,
+  },
+  {
+    name: "Spaceport",
+    description:
+      "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth's rotation for launch.",
+    image: capsule,
+  },
+];
+
+export default function CustomTechnologySwipper() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const swiperRef = useRef<SwiperType | null>(null);
+
+  const handleSlideChange = (swiper: SwiperType) => {
+    setCurrentIndex(swiper.realIndex);
+  };
+
+  const handleButtonClick = (index: number) => {
+    swiperRef.current?.slideToLoop(index);
+  };
+
+  return (
+    <div className="w-full flex flex-col lg:flex-row justify-center items-center">
+      <div className="flex lg:flex-col flex-row gap-7 justify-center items-center mb-10 lg:mb-0 lg:me-6">
+        {crew.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => handleButtonClick(index)}
+            className={`${
+              currentIndex === index
+                ? "bg-white text-black"
+                : "bg-transparent border-2 text-white"
+            } font-bold py-4 px-6 rounded-full cursor-pointer transition-all duration-300`}
+          >
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        onSlideChange={handleSlideChange}
+        className="w-full"
+      >
+        {crew.map((member) => (
+          <SwiperSlide key={member.name}>
+            <div className="text-center lg:ps-8 flex flex-col lg:flex-row justify-center items-center lg:items-start lg:justify-between w-full">
+              <div className="flex flex-col text-white lg:w-1/2 w-full text-center lg:text-start justify-center items-center lg:items-start lg:justify-start lg:px-6">
+                <p className="uppercase text-2xl text-[#D0D6F9] font-barlow">
+                  THE TERMINOLOGY…
+                </p>
+                <h3 className="lg:text-3xl text-white text-6xl font-bellefair font-bold uppercase my-10">
+                  {member.name}
+                </h3>
+                <p className="text-[#D0D6F9] lg:text-lg font-barlow mb-10">
+                  {member.description}
+                </p>
+              </div>
+              <div className=" relative w-2/3 !h-full">
+                <Image
+                  src={member.image}
+                  alt={member.name}
+                  width={200}
+                  height={800}
+                  className="object-contain w-full h-full"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
--- src/app/technology/page.tsx
+++ src/app/technology/page.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import React from "react";
 import desktopBg from "@/../public/technology/background-technology-desktop.jpg";
 import mobileBg from "@/../public/technology/background-technology-mobile.jpg";
+import CustomTechnologySwipper from "./CustomTechnologySwipper";
 
 export default function page() {
   return (
-    <section className="font-sans w-full min-h-screen ">
-      <div className="flex flex-col row-start-2 items-center sm:items-start lg:h-screen w-full">
+    <section className="font-sans w-full min-h-screen">
+      <div className="pt-36 lg:h-screen w-full relative flex flex-wrap justify-center items-center">
         <Image
           src={desktopBg}
           alt="Home Background"
@@ -19,6 +20,14 @@ export default function page() {
           className="absolute inset-0 object-cover w-full h-screen -z-10 lg:hidden"
           fill
         />
+        <div className="md:w-2/3 w-11/12 flex flex-col md:items-start md:justify-start items-center justify-center">
+          <p className="text-white px-6 font-barlow tracking-widest uppercase text-3xl mb-20">
+            <span className="opacity-50">03</span> space launch 101
+          </p>
+          <div className="w-full flex justify-center">
+            <CustomTechnologySwipper />
+          </div>
+        </div>
       </div>
     </section>
   );
